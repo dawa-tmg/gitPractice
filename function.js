f72158d0efa0e98ae7272a5ccac0cdf893db08b0
dawa.t <dawadorji295@gmail.com>
2025-06-30 12:25:28 +0600

    Adds replace on the string

--- function.js
+++ function.js
@@ -2,3 +2,8 @@ function reverse(str){
     return str.split('').reverse().join('')
 }
 console.log(reverse('string'));
+
+function replace(str, charToReplace, strReplacement){
+    return str.replace(charToReplace, strReplacement)
+}
+console.log(replace('spring', 'p', 't'))
