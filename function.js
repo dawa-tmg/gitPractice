33a9f43ff0aea7d37eab7909ed2af2c3e1eac9d1
dawa.t <dawadorji295@gmail.com>
2025-06-30 12:28:54 +0600

    Adds boolean if the length of the string is even

--- function.js
+++ function.js
@@ -7,3 +7,12 @@ function replace(str, charToReplace, strReplacement){
     return str.replace(charToReplace, strReplacement)
 }
 console.log(replace('spring', 'p', 't'))
+
+function strLengthEven(str){
+    if(str.length % 2 === 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(strLengthEven('string'))
