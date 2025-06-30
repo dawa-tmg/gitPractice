8ba1bc627dc22968d680ed64dfee304187564435
dawa.t <dawadorji295@gmail.com>
2025-06-30 12:34:10 +0600

    Adds function to find the index of element in array

--- function.js
+++ function.js
@@ -4,7 +4,7 @@ function reverse(str){
 console.log(reverse('string'));
 
 function replace(str, charToReplace, strReplacement){
-    return str.replace(charToReplace, strReplacement)
+    return str.replace(charToReplace, strReplacement);
 }
 console.log(replace('spring', 'p', 't'))
 
@@ -16,3 +16,14 @@ function strLengthEven(str){
     }
 }
 console.log(strLengthEven('string'))
+
+
+const listOfAnimal = ['dog', 'cat', 'rabbit', 'cow']
+function findRabbit(arr){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === "rabbit"){
+            return arr.indexOf('rabbit');
+        }
+    }
+}
+console.log(findRabbit(listOfAnimal))
