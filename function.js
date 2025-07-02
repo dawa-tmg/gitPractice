803e6762e262c172211c8bd36382ab931cc27f55
dawa.t <dawadorji295@gmail.com>
2025-07-02 10:48:41 +0600

    Adds function to Get ASCII value of a character

--- function.js
+++ function.js
@@ -176,3 +176,7 @@ function countPositivesSumNegatives(input) {
 }
 console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
 
+function getASCII(c){
+  return c.charCodeAt()
+}
+console.log(getASCII('A'))
